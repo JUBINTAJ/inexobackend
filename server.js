54c9c09e307bf92dfde5a2887f0fa33737780454
unknown <jubintajj@gmail.com>
2025-05-09 14:35:21 +0530

    final'

--- server.js
+++ server.js
@@ -21,14 +21,6 @@ const corsOptions = {
   methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"], 
 };
 app.use(cors(corsOptions));
-// app.options("*", cors(corsOptions));
-
-// app.use(cors(
-//   {
-//     origin:'https://inexofrontend.vercel.app',
-//     credentials:true
-//   }
-// ));
 
 
 
